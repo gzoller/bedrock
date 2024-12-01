cfc2a4b57f10866da4fd3557fc9639e9df3a98c1
Greg Zoller <gzoller@blocke.co>
2024-12-01 15:56:02 -0600

    Add injectable data store

---
+++ src/main/scala/com.me/Authentication.scala
@@ -0,0 +1,53 @@
+package com.me
+
+import java.time.Clock
+import scala.util.Try
+import zio._
+import zio.http._
+import pdi.jwt.{Jwt, JwtAlgorithm, JwtClaim}
+import com.github.plokhotnyuk.jsoniter_scala.core._
+import com.github.plokhotnyuk.jsoniter_scala.macros._
+
+object Authentication:
+  implicit val clock: Clock = Clock.systemUTC
+
+  // Define a case class to parse the payload
+  case class JwtPayload(sub: Option[String])
+  implicit val payloadCodec: JsonValueCodec[JwtPayload] = JsonCodecMaker.make
+
+  // Secret Authentication key. In real life this would be a secret in Github or similar
+  val SECRET_KEY = "secretKey"
+  val USER_ID = "bogus_user" // In real life this would be obtained on a login web form or similar
+
+  def jwtEncode(username: String, key: String): String =
+    Jwt.encode(JwtClaim(subject = Some(username)).issuedNow.expiresIn(300), key, JwtAlgorithm.HS512)
+
+  def jwtDecode(token: String, key: String): Try[JwtClaim] =
+    Jwt.decode(token, key, Seq(JwtAlgorithm.HS512))
+
+  // Upon decoding, the subject (containing user id) is oddly stuffed into the payload as JSON
+  // We must pull this out
+  private def extractSubjectFromPayload(claim: JwtClaim): Option[String] = {
+    try {
+      // Deserialize the content field into JwtPayload
+      val payload = readFromString[JwtPayload](claim.content)
+      payload.sub
+    } catch {
+      case _: Throwable => None
+    }
+  }
+
+  val bearerAuthWithContext: HandlerAspect[Any, String] =
+    HandlerAspect.interceptIncomingHandler(Handler.fromFunctionZIO[Request] { request =>
+      request.header(Header.Authorization) match {
+        case Some(Header.Authorization.Bearer(token)) =>
+          ZIO
+            .fromTry(jwtDecode(token.value.asString, SECRET_KEY))
+            .orElseFail(Response.badRequest("Invalid or expired token!"))
+            .flatMap(claim => ZIO.fromOption(extractSubjectFromPayload(claim)).orElseFail(Response.badRequest("Missing subject claim!")))
+            .map(u => (request, u))
+
+        case _ => ZIO.fail(Response.unauthorized.addHeaders(Headers(Header.WWWAuthenticate.Bearer(realm = "Access"))))
+      }
+    })
+
--- src/main/scala/com.me/Main.scala
+++ src/main/scala/com.me/Main.scala
@@ -1,8 +1,8 @@
 package com.me
 
 import zio.*
+import zio.Console.*
 import zio.http.*
-import zio.http.Header.{AccessControlAllowOrigin, AccessControlAllowMethods, AccessControlAllowHeaders}
 
 class EmptyListException(message: String) extends Exception(message)
 
@@ -15,23 +15,38 @@ object Main extends ZIOAppDefault:
       if (strings.isEmpty) 
         ZIO.fail(new EmptyListException("List is empty"))
       else 
-        ZIO.succeed {
-          strings.foreach(str => println(s"${config.prefix} $str"))
-          strings.length
-        }
+        ZIO.foreach(strings)(str => printLine(s"${config.prefix} $str")) // print using ZIO.Console
+          .mapError(_ => new EmptyListException("IO Error")) // Console may toss an IO Exception so need to map that to an EmptyListExcpetion, which is what is expected
+          .as(strings.length) // resulting Int
+
     }
 
-  val program: ZIO[Config & Scope, EmptyListException, Unit] = for {
-    count <- fn(List("Hello", "world", "from", "ZIO"))
-    _ <- ZIO.succeed(println(s"Number of strings printed: $count"))
-    shutdownPromise <- Promise.make[Nothing, Unit]
-    server <- Server.serve(MyRestService.routes).provide(Server.default).fork
-    _ <- shutdownPromise.await.onInterrupt(server.interrupt)
-  } yield ()
+  val program: ZIO[Config & Scope & BookRepo, EmptyListException, Unit] = 
+    // Set up SSL certs, config, and port for use when we start the server.
+    val sslConfig = SSLConfig.fromResource("server.crt", "server.key")
+    val serverConfig = (config: Server.Config) => config.port(8073).ssl(sslConfig)
+    val serverLayer = Server.defaultWith(serverConfig)
+    for {
+      count <- fn(List("Hello", "world", "from", "ZIO"))
+      _ <- ZIO.succeed(println(s"Number of strings printed: $count"))
+
+      // Get the injected BookRepo dependency
+      bookRepo <- ZIO.service[BookRepo]
+      bookService = BookService(bookRepo)
+
+      shutdownPromise <- Promise.make[Nothing, Unit]
+      server <- Server.serve(bookService.routes).provide(serverLayer).fork
+      _ <- shutdownPromise.await.onInterrupt(server.interrupt)
+    } yield ()
 
   val configs = List(Config(prefix = "Prefix:"), Config(prefix = "Blah:"))
 
   override def run: ZIO[Any & ZIOAppArgs & Scope, Any, Any] = {
     val config = configs(scala.util.Random.nextInt(configs.length))
-    program.provideSomeLayer[Scope](ZLayer.succeed(config)).exitCode
+    program
+      .provideSomeLayer[Scope](
+        ZLayer.succeed(BookRepoStd) ++ 
+        ZLayer.succeed(config)
+        )
+      .exitCode
   }
